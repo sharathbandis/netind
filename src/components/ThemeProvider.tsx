d8a80d31b34ff98513640203a3a9c0afebeeb7b1
Sharath <sharathbandi8@gmail.com>
2026-04-23 19:09:56 +0530

    Fix next-themes import path

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -2,7 +2,8 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+// THE FIX: We import the types directly from the main package now!
+import { type ThemeProviderProps } from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
